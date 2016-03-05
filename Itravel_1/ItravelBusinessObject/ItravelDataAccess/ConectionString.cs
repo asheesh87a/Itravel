a92cf56da7a315ff7530113abc4a6e2184074d46
asheesh87a <asheesh87a@gmail.com>
2016-03-05 13:59:56 +0530

    This The Project
    By Asheesh

---
+++ Itravel_1/ItravelBusinessObject/ItravelDataAccess/ConectionString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ItravelDataAccess
+{
+   public class ConectionString
+    {
+        public string dataConection()
+        {
+            string con = @"Data Source=ASHEESHYADAV-PC;Initial Catalog=Itravel;Integrated Security=True";
+            return con;
+        }
+    }
+}
---
+++ Itravel_1/ItravelBusinessObject/ItravelDataAccess/DataConection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ItravelDataAccess
+{
+    public class DataConection
+    {
+        public DataSet DataAccess(string query)
+        {
+            ConectionString cs = new ConectionString();
+            string con = cs.dataConection();
+            SqlConnection sqlcon = new SqlConnection(con);
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            return ds;
+        }
+    }
+}
